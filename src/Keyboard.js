d9b29a49b4f55a95258c90e31e9b3283f4a03873
Leytek <leytek@yandex.ru>
2022-05-13 00:44:22 +0300

    feat: improve button actions

--- src/Keyboard.js
+++ src/Keyboard.js
@@ -3,24 +3,28 @@ import keys, { rusKeys } from './keys';
 export default class Keyboard {
   #keyboard;
   #textarea;
+  #capsLock;
   #mode;
   #lang;
+  #cursor;
 
   constructor() {
     this.#keyboard = document.querySelector('.keyboard');
     this.#textarea = document.querySelector('textarea');
+    this.#cursor = this.#textarea.selectionStart;
 
     this.#lang = localStorage.getItem('lang') ?? 'en';
 
     this.#render('caseDown');
 
+    this.#textarea.addEventListener('click', (e) => this.#handleTextarea(e));
     this.#keyboard.addEventListener('mousedown', (e) => this.#handleClick(e));
     document.addEventListener('keydown', (e) => this.#handleKeyDown(e));
     document.addEventListener('keyup', (e) => this.#handleKeyUp(e));
   }
 
   #renderKeys(e) {
-    if((e.getModifierState('CapsLock') && !e.getModifierState('Shift')) || (!e.getModifierState('CapsLock') && e.getModifierState('Shift'))) {
+    if((this.#capsLock && !e.getModifierState('Shift')) || (!this.#capsLock && e.getModifierState('Shift'))) {
       this.#render('caseUp');
     } else {
       this.#render('caseDown');
@@ -43,12 +47,16 @@ export default class Keyboard {
 
   #handleClick(e) {
     if(e.target.nodeName === 'BUTTON') {
+      if(e.target.classList.contains('CapsLock')) {
+        this.#capsLock = !this.#capsLock;
+        e.target.classList.toggle('active');
+        this.#renderKeys(e);
+      }
       this.#renderTextarea(e.target.innerText);
     }
   }
 
   #handleKeyDown(e) {
-    e.preventDefault();
     if(e.getModifierState('Control') && e.getModifierState('Alt')) {
       if(this.#lang === 'en') {
         this.#lang = 'ru';
@@ -56,21 +64,43 @@ export default class Keyboard {
         this.#lang = 'en';
       }
     }
-    this.#keyboard.childNodes.forEach((node) => node.classList.contains(e.code) && node.classList.add('active'));
+    if(e.code === 'CapsLock') {
+      this.#capsLock = e.getModifierState('CapsLock');
+    }
+    this.#keyboard.childNodes.forEach((node) => node.classList.contains(e.code) && (e.preventDefault(), node.classList.add('active')));
     this.#renderKeys(e);
     this.#renderTextarea(e.key);
     localStorage.setItem('lang', this.#lang);
   }
 
   #handleKeyUp(e) {
-    e.preventDefault();
-    this.#keyboard.childNodes.forEach((node) => node.classList.contains(e.code) && node.classList.remove('active'));
+    this.#keyboard.childNodes.forEach((node) => node.classList.contains(e.code) && !(node.classList.contains('CapsLock') && this.#capsLock) && node.classList.remove('active'));
     this.#renderKeys(e);
   }
 
+  #handleTextarea() {
+    this.#cursor = this.#textarea.selectionStart;
+  }
+
   #renderTextarea(key) {
-    if(key.length === 1 && key !== '←' && key !== '▲' && key !== '◄' && key !== '▼' && key !== '►') {
-      this.#textarea.textContent += key;
+    let val = this.#textarea.value;
+    console.log(this.#cursor);
+    if(key.length === 1 && key !== '←') {
+      val = `${val.slice(0, this.#cursor)}${key}${val.slice(this.#cursor)}`;
+      this.#cursor += 1;
+    } else if((key === '←' || key === 'Backspace') && this.#cursor > 0) {
+      val = val.slice(0, this.#cursor - 1) + val.slice(this.#cursor);
+      this.#cursor -= 1;
+    } else if(key === 'Delete') {
+      val = val.slice(0, this.#cursor) + val.slice(this.#cursor + 1);
+    } else if(key === 'Tab') {
+      val = `${val.slice(0, this.#cursor)}\t${val.slice(this.#cursor)}`;
+      this.#cursor += 1;
+    } else if(key === 'Enter') {
+      val = `${val.slice(0, this.#cursor)}\n${val.slice(this.#cursor)}`;
+      this.#cursor += 1;
     }
+    this.#textarea.value = val;
+    this.#textarea.setSelectionRange(this.#cursor, this.#cursor);
   }
 }
